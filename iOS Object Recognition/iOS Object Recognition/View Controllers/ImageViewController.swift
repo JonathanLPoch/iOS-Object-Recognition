981432f246dcbde9b9cd5e9f04f61d8c209efb64
Jonathan Poch <jonathan.poch@nyu.edu>
2017-07-19 12:56:22 -0400

    Project setup

---
+++ iOS Object Recognition/iOS Object Recognition/View Controllers/ImageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iOS Object Recognition
+//
+//  Created by Jonathan Poch on 7/19/17.
+//  Copyright © 2017 Jonathan Poch. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    view.backgroundColor = UIColor.white
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
